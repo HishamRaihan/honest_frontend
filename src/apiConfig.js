99259b94838ff09b86062e58e518da2a7e437667
hishamraihan <mr.hraihan@gmail.com>
2022-03-09 23:42:26 -0500

    deployment

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://dry-tundra-83070.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
