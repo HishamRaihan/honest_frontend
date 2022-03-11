03a67cd87bf8d11cd88a2f7ea5dfb6acbda8c2f8
hishamraihan <mr.hraihan@gmail.com>
2022-03-11 15:36:48 -0500

    complete just need home text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,27 +5,32 @@ import Container from 'react-bootstrap/Container'
 import { Link, NavLink } from 'react-router-dom'
 import squeege from '../assets/squeege.png'
 // import { AiOutlineSetting } from 'react-icons/ai'
-import { FcHome } from 'react-icons/fc'
+import { FcHome, FcList, FcLibrary } from 'react-icons/fc'
+import { AiOutlineLogout } from 'react-icons/ai'
+import { MdOutlineChangeCircle, MdOutlineCoffeeMaker, MdLogin } from 'react-icons/md'
+import { CgUserList } from 'react-icons/cg'
+import { FaCashRegister } from 'react-icons/fa'
+// import { FaAngellist } from 'react-icons/fa';
 import './Header.scss'
 
 const authenticatedOptions = (
   <>
-    <NavLink to='/jobs/owner' className='nav-link '> Your Jobs </NavLink>
-    <NavLink to='/jobs/create' className='nav-link '>Create Job</NavLink>
-    <NavLink to='/users/' className='nav-link makePost'>List of Users </NavLink>
-    <NavLink to='/jobs' className='nav-link '>All Jobs</NavLink>
+    <NavLink to='/jobs/owner' className='nav-link '> <FcLibrary title='Users Jobs'/> </NavLink>
+    <NavLink to='/jobs/create' className='nav-link '><MdOutlineCoffeeMaker title='Make a job'/></NavLink>
+    <NavLink to='/users/' className='nav-link '><CgUserList title='All Users'/> </NavLink>
+    <NavLink to='/jobs' className='nav-link '><FcList title='All jobs list'/></NavLink>
     {/* <FcEngineering />
     <AiOutlineSetting className='settings' /> */}
-    <NavLink to='/change-password' className='nav-link'>Change Password</NavLink>
-    <NavLink to='/sign-out' className='nav-link'>Sign Out</NavLink>
+    <NavLink to='/change-password' className='nav-link'><MdOutlineChangeCircle title='Change Password'/></NavLink>
+    <NavLink to='/sign-out' className='nav-link'><AiOutlineLogout title='Sign Out'/></NavLink>
 
   </>
 )
 
 const unauthenticatedOptions = (
   <>
-    <NavLink to='/sign-up' className='nav-link'>Register</NavLink>
-    <NavLink to='/sign-in' className='nav-link'>Sign In</NavLink>
+    <NavLink to='/sign-up' className='nav-link'><FaCashRegister title='Register'/></NavLink>
+    <NavLink to='/sign-in' className='nav-link'><MdLogin title='Login'/></NavLink>
   </>
 )
 
@@ -48,7 +53,7 @@ const Header = ({ user }) => (
       <Navbar.Collapse id='basic-navbar-nav'>
         <Nav className='ms-auto' collapseOnSelect>
           {user && (
-            <span className='navbar-text me-2'>Welcome, {user.username}</span>
+            <h5 className='navbar-text me-5'>Welcome, {user.username}</h5>
           )}
           {alwaysOptions}
           {user ? authenticatedOptions : unauthenticatedOptions}
